b66f6f24abf0bb66a8e517726ac8ba0aece74b0f
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-22 16:50:28 +0530

    feat: init with basic updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        blueprint: "#3d6dd7"
+      }
     },
   },
   plugins: [],
